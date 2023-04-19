60de09bb38757c23239832072ef06c74f96a607b
Kevin Renskers <kevin@loopwerk.io>
2023-04-19 12:09:02 +0200

    Initial commit

---
+++ src/lib/Image.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let src: string;
+  export let alt = "";
+
+  const mediaUrl = "https://www.example.com";
+
+  function media(path: string) {
+    console.log(path);
+
+    if (path.startsWith("http")) {
+      return path;
+    } else {
+      return `${mediaUrl}${path}`;
+    }
+  }
+</script>
+
+<img src={media(src)} {alt} />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import image from "$lib/assets/image.jpg";
+  import Image from "$lib/Image.svelte";
+</script>
+
+<img src={image} alt="kitten" />
+
+<Image src={image} alt="kitten" />
